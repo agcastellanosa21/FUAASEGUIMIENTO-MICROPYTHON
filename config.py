384308623a30b3b626063dc5e76618c298b28935
andres.castellanos <andres.castellanos@keyrus.com>
2021-07-29 07:25:33 -0500

    Settings for location update route and vehicle id are added

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 class Config:
     __value = {
         "wifiName": "TIGO-2632",
-        "wifiPass": "4D9697502082"
+        "wifiPass": "4D9697502082",
+        "vehicle": "WTO304",
+        "updateGeoUrl": "http://192.168.1.5:8001/create-localization"
     }
 
     @staticmethod
